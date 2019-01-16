b9fd5a9f23bc3177046559eb431037593640565f
Milan Drenovac <milandrenovac@gmail.com>
2019-01-16 13:57:04 -0800

    tst 2

---
+++ welcome.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+
+<?php
+$var1 = $_GET["A"];
+$var2 = $_GET["B"];
+?>
+
+<?php
+if (is_numeric($var1) || is_numeric($var2))
+{
+ echo 'A: ',$var1,'<br>';
+  echo 'B: ',$var2,'<br>';
+  echo 'Addition: ' ,$var1+$var2,'<br>';
+  echo 'Subtraction: ',$var1-$var2,'<br>';
+  echo 'Multiplication: ',$var1*$var2,'<br>';
+  echo 'Division: ',$var1/$var2,'<br>';
+  }
+?>
+</body>
+
+
+
+</html>
